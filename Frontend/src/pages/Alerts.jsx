be0d782eaf580b4a4bda5feb368202963cc815dd
PiyushMahore <156774805+PiyushMahore@users.noreply.github.com>
2025-05-14 12:14:54 +0530

    Update Alerts component: enhance layout and replace images for better visuals

--- Frontend/src/pages/Alerts.jsx
+++ Frontend/src/pages/Alerts.jsx
@@ -9,30 +9,21 @@ function Alerts() {
             </div>
             <div className='flex justify-center flex-wrap gap-8 mt-14 p-1 text-center'>
 
-                <div className="animation sm:w-[30%] relative inline-block rounded-tl-3xl rounded-bl-3xl border-y border-l dark:border-white border-gray-900 py-3 pl-3">
+                <div className="h-[500px] overflow-hidden animation sm:w-[30%] relative inline-block rounded-tl-3xl rounded-bl-3xl border-y border-l dark:border-white border-gray-900 py-3 pl-3">
                     <div className='rounded-3xl radial-gradient-custom dark:border-gray-300 border-gray-900 border overflow-hidden shadow-xl dark:shadow-gray-500 shadow-gray-900'>
                         <div className='absolute w-full -translate-x-[50%] top-12 left-[40%]'>
                             <h3 className='text-4xl'>On-call scheduling</h3>
                         </div>
-                        <img className='w-full h-auto' src="https://betterstack.com/assets/v2/uptime/on-call-scheduling-2fa5a57807fb93898f50a2dc8878e0dba8223329cf2f3ab765ff5922920aa96e.png" alt="" />
+                        <img className='w-full h-full bg-cover bg-center bg-no-repeat' src="https://betterstack.com/assets/v2/homepage-v3/metrics-01a8050d05f63c1cd782660389d468bfa203f5e409a174d4e784aff6250fdbea.jpg" alt="" />
                     </div>
                 </div>
 
-                <div className="animation sm:w-[30%] text-gray-800 relative inline-block rounded-tl-3xl rounded-bl-3xl">
-                    <div className='rounded-3xl h-[470px] radial-gradient-custom dark:border-gray-300 border-gray-900 border overflow-hidden shadow-xl dark:shadow-gray-500 shadow-gray-900'>
-                        <div className='absolute w-full -translate-x-[50%] top-12 left-[50%]'>
-                            <h3 className='text-4xl'>Unlimited voice call alerts</h3>
-                        </div>
-                        <img className='w-full h-[500px]' src="https://betterstack.com/assets/v2/uptime/call-alerts-cde9f33376337c149badf3b40df6c16037fefc65be02f0b5c90aa53447fbcf88.jpg" alt="" />
-                    </div>
-                </div>
-
-                <div className="animation sm:w-[30%] relative inline-block rounded-tr-3xl rounded-br-3xl border-y border-r dark:border-white border-gray-900 py-3 pr-3">
+                <div className="animation sm:w-[60%] relative inline-block rounded-tr-3xl rounded-br-3xl border-y border-r dark:border-white border-gray-900 py-3 pr-3">
                     <div className='rounded-3xl h-[454px] radial-gradient-custom dark:border-gray-300 border-gray-900 border overflow-hidden shadow-xl dark:shadow-gray-500 shadow-gray-900'>
                         <div className='absolute w-full -translate-x-[50%] top-12 left-[40%] overflow-hidden'>
                             <h3 className='text-4xl'>On-call scheduling</h3>
                         </div>
-                        <img className='w-full h-auto bg-cover bg-center sm:relative -top-14' src="https://betterstack.com/assets/v2/uptime/flexible-escalations-31c852dd631e16846a2371f215fc6fe09a669404c908ebfab69076f3fb8287c0.png" alt="" />
+                        <img className='w-full h-auto bg-cover bg-center sm:relative -top-14' src="https://betterstack.com/assets/v2/homepage-v3/anomaly-detection-faa34ba05334a907780d43f42df9ebedfac460a7f6509482d555921dae82d13b.jpg" alt="" />
                     </div>
                 </div>
 
