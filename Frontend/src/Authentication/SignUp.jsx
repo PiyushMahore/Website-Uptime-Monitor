8ac305d9e365cb05c8d9aaf62c6e598eca9e93e8
PiyushMahore <156774805+PiyushMahore@users.noreply.github.com>
2025-05-16 21:31:53 +0530

    Add maxLength validation to mobile number input in SignUp form

--- Frontend/src/Authentication/SignUp.jsx
+++ Frontend/src/Authentication/SignUp.jsx
@@ -148,6 +148,7 @@ function SignUp() {
                                     name="mobileNumber"
                                     type="tel"
                                     required
+                                    maxLength={10}
                                     className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-200"
                                 />
                             </div>
